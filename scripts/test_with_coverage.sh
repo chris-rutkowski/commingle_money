67b22b62a2290733808d69d1e3352e20ba0c7745
Chris Rutkowski <chris@rkw.ski>
2025-07-12 15:48:51 +0400

    no message

--- scripts/test_with_coverage.sh
+++ scripts/test_with_coverage.sh
@@ -1,6 +1,6 @@
 # brew install lcov
 # run: ./test_with_coverage.sh
 
-flutter test --coverage
+flutter test test --coverage
 genhtml coverage/lcov.info -o coverage/html
 open coverage/html/index.html
